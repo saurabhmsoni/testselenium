3cc5fa8e13c1ce549a645f0ded84c506da010d20
saurabh.soni <saurabh.soni@hightail.com>
2016-01-12 21:37:01 -0800

    firstTest commit

---
+++ src/hightailfront/FirstTest.java
@@ -0,0 +1,38 @@
+package hightailfront;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstTest {
+
+	
+	
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FirstTest obj = new FirstTest();
+		//obj.callFirefox();
+		obj.callChrome();
+
+	}
+	
+	public void  callFirefox(){
+		WebDriver driver = new FirefoxDriver();
+		String baseUrl ="https://www.hightail.com";
+		driver.get(baseUrl);
+		
+	}
+	
+	public void  callChrome(){
+		WebDriver driver;
+		String baseUrl ="https://www.hightail.com";
+		System.setProperty("webdriver.chrome.driver", "/Users/saurabh.soni/Downloads/chromedriver");
+		driver = new ChromeDriver();
+		driver.get(baseUrl);
+		
+	}
+
+}

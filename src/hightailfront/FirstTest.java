a5fe84dd35ec5af1af07421ffe5d172801b29227
saurabh.soni <saurabh.soni@hightail.com>
2016-01-12 21:41:13 -0800

    removing space

--- src/hightailfront/FirstTest.java
+++ src/hightailfront/FirstTest.java
@@ -6,11 +6,6 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FirstTest {
 
-	
-	
-
-	
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		FirstTest obj = new FirstTest();
